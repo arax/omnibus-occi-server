656eeacf3b663267360b0ffb664baf2eacd11fbf
Boris Parak <parak@cesnet.cz>
2014-03-12 00:34:20 +0100

    Added dep on the latest rake gem

--- config/software/occi-server.rb
+++ config/software/occi-server.rb
@@ -8,6 +8,7 @@ build do
   command "git clone https://github.com/EGI-FCTF/rOCCI-server /opt/occi-server/app"
   command "cd /opt/occi-server/app && git checkout master"
   gem "install bundler -n #{install_dir}/bin --no-rdoc --no-ri"
+  gem "install rake -n #{install_dir}/bin --no-rdoc --no-ri"
   command "cd /opt/occi-server/app && bundle install --deployment"
 end
 
