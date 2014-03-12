648f4f0ff4a295358786042c9b3304da8235dec0
Boris Parak <parak@cesnet.cz>
2014-03-12 08:56:22 +0100

    Updated dep installation w/ bundler

--- config/software/occi-server.rb
+++ config/software/occi-server.rb
@@ -4,11 +4,17 @@ default_version "1.0.0.beta.2"
 dependency "ruby"
 dependency "rubygems"
 
+env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+}
+
 build do
-  command "git clone https://github.com/EGI-FCTF/rOCCI-server /opt/occi-server/app"
-  command "cd /opt/occi-server/app && git checkout master"
+  command "git clone https://github.com/EGI-FCTF/rOCCI-server #{install_dir}/embedded/app"
+  command "cd #{install_dir}/embedded/app && git checkout master"
   gem "install bundler -n #{install_dir}/bin --no-rdoc --no-ri"
   gem "install rake -n #{install_dir}/bin --no-rdoc --no-ri"
-  command "cd /opt/occi-server/app && bundle install --deployment"
+  bundle "install --deployment --without development test --path=#{install_dir}/embedded/app/vendor/bundle", :env => env
 end
 
