85aab4a0339443d5faaa2cf81d7c59f0ddb6d0ec
Boris Parak <parak@cesnet.cz>
2014-04-28 23:07:26 +0200

    Minor version bump to rOCCI-server v1.0.2

--- config/projects/occi-server.rb
+++ config/projects/occi-server.rb
@@ -4,7 +4,7 @@ homepage "https://github.com/EGI-FCTF/rOCCI-server"
 
 replaces        "occi-server"
 install_path    "/opt/occi-server"
-build_version   "1.0.1"
+build_version   "1.0.2"
 build_iteration 1
 
 # creates required build directories
