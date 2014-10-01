c83e17daea5db2ad5c4932d9284cfbc425267343
Boris Parak <parak@cesnet.cz>
2014-10-01 20:50:42 +0200

    Introduced external runtime dependencies

--- config/projects/occi-server.rb
+++ config/projects/occi-server.rb
@@ -16,5 +16,15 @@ dependency "version-manifest"
 # occi-server dependencies/components
 dependency "occi-server"
 
+# add external (runtime) dependencies/services
+external_deps = if File.exists?('/etc/redhat-release')
+                  # we are on CentOS/SL
+                  %w(gcc-c++ curl-devel httpd httpd-devel apr-devel apr-util-devel mod_ssl policycoreutils-python mod_security memcached openssl-devel zlib-devel git)
+                else
+                  # we are in Debian/Ubuntu
+                  %w(apache2 libapache2-mod-passenger libapache2-modsecurity memcached git)
+                end
+external_deps.each { |ext_dep| runtime_dependency ext_dep }
+
 exclude "\.git*"
 exclude "bundler\/git"
