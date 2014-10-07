bcead45220f0db1c5eebee91c45d7cd28c14bcf3
Boris Parak <parak@cesnet.cz>
2014-10-07 11:45:43 +0200

    Omnibus 4.0.0.rc1

---
+++ config/projects/occi-server.rb
@@ -0,0 +1,34 @@
+name "occi-server"
+maintainer "parak@cesnet.cz"
+homepage "https://github.com/EGI-FCTF/rOCCI-server"
+
+# Defaults to C:/occi-server on Windows
+# and /opt/occi-server on all other platforms
+install_dir "#{default_root}/#{name}"
+build_version "1.1.0.beta.2"
+build_iteration 1
+
+override :rubygems, :version => '2.4.1'
+override :ruby, :version => '2.1.3'
+
+# Creates required build directories
+dependency "preparation"
+
+# occi-server dependencies/components
+dependency "occi-server"
+
+# Version manifest file
+dependency "version-manifest"
+
+# add external (runtime) dependencies/services
+external_deps = if File.exists?('/etc/redhat-release')
+                  # we are on CentOS/SL
+                  %w(gcc-c++ curl-devel httpd httpd-devel apr-devel apr-util-devel mod_ssl policycoreutils-python mod_security memcached openssl-devel zlib-devel git)
+                else
+                  # we are in Debian/Ubuntu
+                  %w(apache2 libapache2-mod-passenger libapache2-modsecurity memcached git)
+                end
+external_deps.each { |ext_dep| runtime_dependency ext_dep }
+
+exclude "**/.git"
+exclude "**/bundler/git"
---
+++ config/software/occi-server.rb
@@ -0,0 +1,23 @@
+name "occi-server"
+default_version "master"
+
+dependency "ruby"
+dependency "rubygems"
+dependency "rsync"
+
+env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+}
+
+source git: "https://github.com/EGI-FCTF/rOCCI-server"
+
+build do
+  gem "install bundler -n #{install_dir}/embedded/bin --no-rdoc --no-ri"
+  gem "install passenger -n #{install_dir}/embedded/bin --no-rdoc --no-ri"
+  bundle "install --deployment --without development test --path=#{install_dir}/embedded/app/vendor/bundle", :env => env
+  command "mkdir -p #{install_dir}/embedded/app/rOCCI-server"
+  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/app/rOCCI-server/"
+  delete "#{install_dir}/embedded/app/rOCCI-server/vendor/bundle"
+end
