fe8293b71a57cde624ab96c73b03913fd54d1a3b
Boris Parak <parak@cesnet.cz>
2014-10-11 13:45:57 +0200

    Switched to rOCCI-server 1.1.x, STABLE

--- config/projects/occi-server.rb
+++ config/projects/occi-server.rb
@@ -5,8 +5,8 @@ homepage "https://github.com/EGI-FCTF/rOCCI-server"
 # Defaults to C:/occi-server on Windows
 # and /opt/occi-server on all other platforms
 install_dir "#{default_root}/#{name}"
-build_version "1.1.0.beta.2"
-build_iteration 2
+build_version "1.1.0"
+build_iteration 1
 
 override :rubygems, :version => '2.4.1'
 override :ruby, :version => '2.1.3'
