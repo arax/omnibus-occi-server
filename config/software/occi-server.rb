76ad12b71e6f60490c95253d738f719d7cb7d46e
Boris Parak <parak@cesnet.cz>
2017-09-07 14:11:23 +0200

    Updated post* scripts to support rOCCI-server v2

--- config/software/occi-server.rb
+++ config/software/occi-server.rb
@@ -16,8 +16,9 @@ source git: 'https://github.com/the-rocci-project/rOCCI-server.git'
 
 build do
   gem "install bundler -n #{install_dir}/embedded/bin --no-rdoc --no-ri"
-  bundle "install --deployment --path=#{install_dir}/embedded/app/vendor/bundle", :env => env
+  bundle "install --deployment --without development test --path=#{install_dir}/embedded/app/vendor/bundle", :env => env
   command "mkdir -p #{install_dir}/embedded/app/rOCCI-server"
   command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git --exclude=.gitignore ./ #{install_dir}/embedded/app/rOCCI-server/"
   delete "#{install_dir}/embedded/app/rOCCI-server/vendor/bundle"
+  delete "#{install_dir}/embedded/app/vendor/bundle/ruby/*/cache/*"
 end
