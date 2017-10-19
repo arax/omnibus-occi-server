4147288b02bb4b3041cea74e41116034d738de23
Boris Parak <parak@cesnet.cz>
2017-10-19 13:50:21 +0200

    Version bump to v2.0.1

--- config/projects/occi-server.rb
+++ config/projects/occi-server.rb
@@ -6,11 +6,11 @@ description 'The rOCCI Framework - Server-side implementation for OpenNebula'
 # Defaults to C:/occi-server on Windows
 # and /opt/occi-server on all other platforms
 install_dir "#{default_root}/#{name}"
-build_version '2.0.0'
-build_iteration 5
+build_version '2.0.1'
+build_iteration 1
 
 override :rubygems, :version => '2.6.8'
-override :ruby, :version => '2.4.1'
+override :ruby, :version => '2.4.2'
 
 # Creates required build directories
 dependency 'preparation'
--- config/software/occi-server.rb
+++ config/software/occi-server.rb
@@ -1,5 +1,5 @@
 name 'occi-server'
-default_version 'v2.0.0'
+default_version 'v2.0.1'
 
 license "Apache-2.0"
 license_file "LICENSE"
